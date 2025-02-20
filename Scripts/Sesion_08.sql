cc687841f2ccdb6eeb97821b996effaa78489f1d
Mata13 <marc.dev121@gmail.com>
2025-02-20 15:11:29 -0600

    Se trabajo con GROUP BY en la Sesion_08

--- Scripts/Sesion_08.sql
+++ Scripts/Sesion_08.sql
@@ -315,3 +315,224 @@ SELECT REPLACE(id, 'MEX', 'mex') FROM datos;
 ------------- Sesión 08 -------------
 -------------------------------------
 
+-- La cláusula GROUP BY divide a los registros
+-- de una tabla, mediante un select, en grupos
+-- La sintaxis es la siguiente:
+-- SELECT columna_1, ..., columna_n, función_de_agregado
+-- (columna a aplicar) FROM nombre_tabla GROUP BY columna_1, ..., columna_n
+
+-- Creamos una nueva tabla
+CREATE TABLE info_paises(continente varchar(10), pais varchar(10), cantidad int);
+
+-- Muestra la tabla sin valores
+SELECT * FROM info_paises
+
+-- Insertamos los valores
+INSERT INTO info_paises VALUES
+	('America', 'Mexico', 575),
+	('Europ', 'España', 975),
+	('Europ', 'Italia', 549),
+	('America', 'Mexico', 933),
+	('Oceania', 'Australia', 653),
+	('Asia', 'Japon', 791),
+	('Asia', 'China', 712),
+	('America', 'Canada', 674),
+	('Asia', 'China', 574),
+	('Africa', 'Madagascar', 854);
+
+-- Muestra la tabla para ver si hay cambios
+SELECT * FROM info_paises;
+
+-- Aplicamos la función suma a la columna cantidad y muestra el resultado
+-- esto es como tener una tabla dinámica con Excel
+SELECT continente, sum(cantidad) AS suma FROM info_paises GROUP BY continente;
+
+-- Podemos hacerlo más complejo
+SELECT continente,
+	SUM(cantidad) AS suma,
+	AVG(cantidad) AS promedio,
+	MAX(cantidad) AS maximo,
+	MIN(cantidad) AS minimo
+FROM info_paises GROUP BY continente;
+
+-- Podemos hacerlo ahora por continente y país 
+-- Muestra la información agrupada por grupos
+SELECT continente, pais,
+	SUM(cantidad) AS suma,
+	AVG(cantidad) AS promedio,
+	MAX(cantidad) AS maximo,
+	MIN(cantidad) AS minimo
+FROM info_paises GROUP BY continente, pais;
+
+-- Conteo, muestra los países que hay en cada continente
+SELECT continente,
+	COUNT(*) 
+FROM info_paises GROUP BY continente;
+
+-- Otra muestra de la información
+SELECT continente,
+	SUM(cantidad) AS suma,
+	AVG(cantidad) AS promedio,
+	MAX(cantidad) AS maximo,
+	MIN(cantidad) AS minimo,
+	COUNT(*) AS total_paises
+FROM info_paises GROUP BY continente;
+
+-- Podemos ordenar la información por suma
+SELECT continente,
+	SUM(cantidad) AS suma,
+	AVG(cantidad) AS promedio,
+	MAX(cantidad) AS maximo,
+	MIN(cantidad) AS minimo,
+	COUNT(*) AS total_paises
+FROM info_paises GROUP BY continente ORDER BY suma;
+
+-- Podemos ordenar la información de mayor a menor usando una columna calculada
+SELECT continente,
+	SUM(cantidad) AS suma,
+	AVG(cantidad) AS promedio,
+	MAX(cantidad) AS maximo,
+	MIN(cantidad) AS minimo,
+	COUNT(*) AS total_paises
+FROM info_paises GROUP BY continente ORDER BY suma DESC;
+
+-- Podemos ordenar la información por total porque hay empates
+-- el criterio de desempate es el promedio
+SELECT continente,
+	SUM(cantidad) AS suma,
+	AVG(cantidad) AS promedio,
+	MAX(cantidad) AS maximo,
+	MIN(cantidad) AS minimo,
+	COUNT(*) AS total_paises
+FROM info_paises GROUP BY continente ORDER BY suma DESC, promedio DESC;
+
+-- Muestra los grupos que hay pero sin función de agregación
+SELECT continente from info_paises GROUP BY continente;
+
+-- Muestra los países que hay en los grupos pero sin función de agregación
+SELECT continente, pais from info_paises GROUP BY continente, pais;
+
+SELECT pais, continente from info_paises GROUP BY pais, continente;
+
+SELECT pais, continente, SUM(cantidad) from info_paises GROUP BY pais, continente;
+
+
+-- La cláusula HAVING es de uso semejante al WHERE
+-- se ocupa únicamente en el caso en que tengamos GROUP BY 
+-- nos devuelve aquellos resultados que cumplen con
+-- ciertas condición lógica 
+
+-- Muestra el resultado usando HAVING y una condición lógica 
+-- promedio > 760
+SELECT continente,
+	SUM(cantidad) AS suma,
+	AVG(cantidad) AS promedio,
+	MAX(cantidad) AS maximo,
+	MIN(cantidad) AS minimo,
+	COUNT(*) AS total_paises
+FROM info_paises
+GROUP BY continente
+HAVING AVG(cantidad) > 760;
+
+
+-- Muestra el resultado usando HAVING y una condición lógica 
+-- promedio > 760
+-- ordenado por suma
+SELECT continente,
+	SUM(cantidad) AS suma,
+	AVG(cantidad) AS promedio,
+	MAX(cantidad) AS maximo,
+	MIN(cantidad) AS minimo,
+	COUNT(*) AS total_paises
+FROM info_paises
+GROUP BY continente
+HAVING AVG(cantidad) > 760
+ORDER BY suma;
+
+-- Muestra la tabla original
+SELECT * FROM info_paises;
+
+-- Muestra el resultado usando WHERE y una condición lógica 
+SELECT * FROM info_paises WHERE cantidad > 729;
+
+
+-- Elimina la tabla de modulo_victimizacion, no elimina la información
+DROP TABLE modulo_victimizacion;
+
+SELECT * FROM modulo_victimizacion;
+
+-- Creamos la tabla de modulo_victimizacion nuevamente
+CREATE TABLE modulo_victimizacion(
+	entidad varchar(2),
+	viv varchar(5),
+	hogar varchar(5),
+	upm varchar(5),
+	renglon varchar(20),
+	tipo_delito int,
+	num_delito int,
+	nombre varchar(50),
+	apellido varchar(50),
+	perdida int
+);
+
+-- Llenamos la tabla usando INSERT
+INSERT INTO modulo_victimizacion 
+	VALUES 
+	('01', '15', '03', '15', '10', 3, 1, 'Luis', 'Miranda', 2000),
+	('01', '15', '03', '15', '10', 3, 2, 'Luis', 'Miranda', 3000),
+	('02', '20', '13', '10', '03', 4, 1, 'Oscar', 'Ruiz', 1000),
+	('09', '16', '11', '05', '01', 5, 1, 'Raul', 'Ortiz', 4500),
+	('32', '12', '11', '02', '04', 2, 1, 'Luisa', 'Ortiz', 300),
+	('32', '12', '11', '02', '04', 3, 1, 'Luisa', 'Ortiz', 500),
+	('05', '18', '08', '20', '15', 2, 1, 'Maria', 'Gonzalez', 1500),
+	('12', '10', '17', '04', '06', 4, 1, 'Pedro', 'Martinez', 2500),
+	('07','21','05','12','09',3, 1, 'Ana', 'López',3500),
+	('19','07','14','08','11',5, 1, 'Juan', 'Díaz',4000),
+	('03','22','09','11','07',2, 1, 'Elena', 'Sánchez',2000),
+	('14','11','12','03','02',3, 1, 'Jorge', 'Pérez',3000),
+	('28','09','20','07','05',4, 1, 'Laura', 'Ramírez',5000),
+	('08','16','18','09','13',2, 1, 'Diego', 'Hernández',1800),
+	('21','13','16','06','01',5, 1, 'Sofía', 'Gómez',4200),
+	('11','17','02','15','10',3, 1, 'Carlos', 'Fernández',2700);
+
+-- Muestra la tabla para ver los cambios
+SELECT * FROM modulo_victimizacion;
+
+-- Contar el número de víctima por tipo de delito y mostrar solo aquellos
+-- con más de una víctima
+SELECT tipo_delito, COUNT(*) 
+FROM modulo_victimizacion
+GROUP BY tipo_delito
+HAVING COUNT(*) > 1;
+
+-- Calcular el promedio de pérdidas por entidad
+-- y mostrar solo aquellas entidades con un promedio de pérdida
+-- superior a 2000
+SELECT entidad, AVG(perdida) AS promedio_perdida 
+FROM modulo_victimizacion
+GROUP BY entidad
+HAVING AVG(perdida) > 2000;
+
+-- Muestra la tabla
+SELECT * FROM modulo_victimizacion;
+
+-- Listar las viviendas y hogares donde se hayan registrado
+-- exactamente dos delitos
+SELECT viv, hogar, nombre, COUNT(*)
+FROM modulo_victimizacion
+GROUP BY viv, hogar, nombre
+HAVING COUNT(*) = 2;
+
+-- Mostrar el número de delitos comeditos por cada apellido y nombre
+SELECT nombre, apellido, COUNT(*)
+FROM modulo_victimizacion
+GROUP BY nombre, apellido;
+
+-- Mostrar el máximo de pérdida por entidad y UPM
+-- ordenado de mayor a menor 
+SELECT entidad, upm, MAX(perdida)
+FROM modulo_victimizacion
+GROUP BY entidad, upm -- upm es como la colonia o fraccionamiento
+ORDER BY max(perdida) DESC;
+
+-- En esta Sesión trabajamos con GROUP BY
